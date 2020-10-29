3b9e1489ee582716157af9cb43ae63763971ffc2
Balvine <balvy075@gmail.com>
2020-10-29 17:13:29 +0300

    iniiated a post method

--- hood/views.py
+++ hood/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse,Http404
+from django.contrib.auth.decoraters import login
 # Create your views here.
+
+@login_required
+def index(request):
+    current user = request.user
+    try:
+        profile = UserProfile.objects.get(user =current_user)
+    except:
+      return redirect('edit.profile',username=current_user=username)
+
+
