16784943afa104878229b001f2eb44448c8f71cd
Balvine <balvy075@gmail.com>
2020-10-31 11:42:45 +0300

    made migrations

--- hood/urls.py
+++ hood/urls.py
@@ -1,7 +1,7 @@
-from django.urls import path, re_path
-from . import views
+# from django.urls import path, re_path
+# from . import views
 
-urlpatterns = [
-    path('',views.index,name = 'index'),
-    re_path('edit_profile/(?P<username>\w{0,50})',views.edit_profile,name = 'edit_profile'),
-]
+# urlpatterns = [
+#     path('',views.index,name = 'index'),
+#     re_path('edit_profile/(?P<username>\w{0,50})',views.edit_profile,name = 'edit_profile'),
+# ]
--- hood/views.py
+++ hood/views.py
@@ -1,36 +1,36 @@
-from django.shortcuts import render,redirect
-from django.http import HttpResponse,Http404,HttpResponseRedirect
-from django.contrib.auth.decorators import login_required
-# Create your views here.
-@login_required
-def index(request):
-    current_user = request.user
-    try:
-        profile = UserProfile.objects.get(user = current_user)
-    except:
-        return redirect('edit_profile',username = current_user.username)
+# from django.shortcuts import render,redirect
+# from django.http import HttpResponse,Http404,HttpResponseRedirect
+# from django.contrib.auth.decorators import login_required
+# # Create your views here.
+# @login_required
+# def index(request):
+#     current_user = request.user
+#     try:
+#         profile = UserProfile.objects.get(user = current_user)
+#     except:
+#         return redirect('edit_profile',username = current_user.username)
 
-    try:
-        posts = Post.objects.filter(neighborhood = profile.neighborhood)
-    except:
-        posts = None
+#     try:
+#         posts = Post.objects.filter(neighborhood = profile.neighborhood)
+#     except:
+#         posts = None
 
-    if request.method == 'POST':
-        form = PostForm(request.POST)
-        if form.is_valid():
-            post = form.save(commit=False)
-            post.user = current_user
-            post.neighborhood = profile.neighborhood
-            post.type = request.POST['type']
-            post.save()
+#     if request.method == 'POST':
+#         form = PostForm(request.POST)
+#         if form.is_valid():
+#             post = form.save(commit=False)
+#             post.user = current_user
+#             post.neighborhood = profile.neighborhood
+#             post.type = request.POST['type']
+#             post.save()
 
-            if post.type == '1':
-                recipients = UserProfile.objects.filter(neighborhood=post.neighborhood)
-                for recipient in recipients:
-                    send_a_email(post.title,post.content,recipient.email)
+#             if post.type == '1':
+#                 recipients = UserProfile.objects.filter(neighborhood=post.neighborhood)
+#                 for recipient in recipients:
+#                     send_a_email(post.title,post.content,recipient.email)
 
-        return redirect('index')
-    else:
-        form = PostForm()
-    return render(request,'index.html',{"posts":posts,"profile":profile,"form":form})
+#         return redirect('index')
+#     else:
+#         form = PostForm()
+#     return render(request,'index.html',{"posts":posts,"profile":profile,"form":form})
 
