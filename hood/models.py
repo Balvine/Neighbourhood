5fd84e7c137522068ea1e46a580f8f00e12a2a06
Balvine <balvy075@gmail.com>
2020-10-31 21:14:04 +0300

    created models

--- hood/models.py
+++ hood/models.py
@@ -1,3 +1,30 @@
 from django.db import models
-
+from django.contrib.auth.models import user
 # Create your models here.
+
+class Neighborhood(models.Model):
+    name = models.CharField(max_length = 50)
+    location = models.ForeignKey('Location',on_delete = models.CASCADE,null = True)
+    admin = models.ForeignKey(User,on_delete = models.CASCADE)
+    occupants = models.IntegerField(null=True)
+
+    def __str__(self):
+        return self.name
+
+    def create_neighborhood(self):
+        self.save()
+
+    def delete_neighborhood(self):
+        self.delete()
+
+    @classmethod
+    def find_neighborhood(cls,neigborhood_id):
+        neighborhood = cls.objects.get(id = neigborhood_id)
+        return neighborhood
+
+    def update_neighborhood(self):
+        self.save()
+
+    def update_occupants(self):
+        self.occupants += 1
+        self.save()
