897d708e12fc744e3586e302dff497242d927d7b
Balvine <balvy075@gmail.com>
2020-11-01 11:04:13 +0300

    created model

--- hood/models.py
+++ hood/models.py
@@ -1,6 +1,6 @@
 from django.db import models
-from django.contrib.auth.models import user
-# Create your models here.
+from django.contrib.auth.models import User
+# # Create your models here.
 
 class Neighborhood(models.Model):
     name = models.CharField(max_length = 50)
@@ -28,3 +28,16 @@ class Neighborhood(models.Model):
     def update_occupants(self):
         self.occupants += 1
         self.save()
+        
+        
+        
+class UserProfile(models.Model):
+    user = models.ForeignKey(User,on_delete = models.CASCADE,related_name = 'user_profile')
+    first_name = models.CharField(max_length = 50,null=True)
+    last_name = models.CharField(max_length = 50,null=True)
+    bio = models.TextField(null=True)
+    neighborhood = models.ForeignKey(Neighborhood,on_delete = models.CASCADE)
+    email = models.EmailField(max_length = 60)
+
+    def __str__(self):
+        return self.user.username
--- hood/views.py
+++ hood/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render,redirect
 from django.http import HttpResponse,Http404,HttpResponseRedirect
+from .models import UserProfile,Neighborhood
 from django.contrib.auth.decorators import login_required
+
+
 # Create your views here.
 @login_required
 def index(request):
